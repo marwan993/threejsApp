90c57e1fdda54f28f379fa7563671a335725aaa8
Marwan Al-Aloosi <marwanal-aloosi@Apples-MacBook-Pro.local>
2023-09-05 01:13:55 -0700

    added shadows, camera adjustments to the index.jsx in canvas

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
-import CameraRig from './Camerarig';
+import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
-      <Environment preset='city'/>
+      <Environment preset="city" />
+
       <CameraRig>
         <Backdrop />
         <Center>
